7b48430cae4b1a9bcba1bf8542f66761613f869c
liviu <liviunossa@gmail.com>
2022-05-13 18:54:50 +0200

    Reset config files

--- client/js/clientConfig.js
+++ client/js/clientConfig.js
@@ -1,7 +1,7 @@
 define(function(){
     var config = {
-        //host:'https://localhost:8000'
-        host:'https://10.50.172.36:8000'
+        host:'https://localhost:8000',
+        s2tHost: 'https://localhost:4000'
     };
     return config;
 });
--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 const config = {
     vcsApi: {
         host:'sandbox.virtualcareservices.net',
-        key: '04hMJ83KXK7d2oQgTCKuuxUx'
+        key: 'YOUR API KEY HERE'
     }
    };
    
