165d03d0b74e4699f96f706a9333a8b8225df341
Liviu <liviunossa@gmail.com>
2022-05-13 22:27:22 +0300

    Fix config files

--- client/js/clientConfig.js
+++ client/js/clientConfig.js
@@ -1,10 +1,7 @@
 define(function(){
     var config = {
-        // host:'https://10.255.100.30:8000',
-        // s2tHost: 'https://10.255.100.30:4000'
-
-        host:'https://192.168.67.60:8000',
-        v2tHost: 'https://192.168.67.60:4000'
+        host:'https://localhost:8000',
+        v2tHost: 'https://localhost:4000'
     };
     return config;
 });
--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 const config = {
     vcsApi: {
         host:'sandbox.virtualcareservices.net',
-        key: '044Zs8hpXkTT1L310HVFilS'
+        key: 'YOUR API KEY HERE'
     }
    };
    
