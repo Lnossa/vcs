667044cbf98bb1b32db43928a0635604a32c2204
Ionut Dirvareanu <99186989+7gajaN@users.noreply.github.com>
2022-10-15 22:34:45 +0300

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,11 +1,7 @@
 const config = {
     vcsApi: {
         host:'sandbox.virtualcareservices.net',
-<<<<<<< Updated upstream
         key: '<YOUR API KEY HERE>'
-=======
-        key: '04hG4dAIIYV60AGppnk5giMm'
->>>>>>> Stashed changes
     }
    };
    
